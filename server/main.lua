9c13fa5fd0dd01ae75759e5dd1166eef9a1d9bfd
DosKit <56743692+DosKit@users.noreply.github.com>
2023-06-16 11:49:13 -0400

    Update main.lua
    Localized the function as well as converted the RegisterServerEvent to RegisterNetEvent without the need for AddEventHandler.

--- server/main.lua
+++ server/main.lua
@@ -1,33 +1,7 @@
-
+local GetPlayer = exports['qbr-core']:GetPlayer
 local DoorInfo	= {}
 
-RegisterServerEvent('qbr-doorlock:updatedoorsv')
-AddEventHandler('qbr-doorlock:updatedoorsv', function(doorID, state, cb)
-    local src = source
-	local Player = exports['qbr-core']:GetPlayer(src)
-	if not IsAuthorized(Player.PlayerData.job.name, Config.DoorList[doorID]) then
-		TriggerClientEvent('QBCore:Notify', src, 9, Lang:t("error.nokey"), 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
-            return
-        else
-            TriggerClientEvent('qbr-doorlock:changedoor', src, doorID, state)
-        end
-end)
-
-RegisterServerEvent('qbr-doorlock:updateState')
-AddEventHandler('qbr-doorlock:updateState', function(doorID, state, cb)
-    local src = source
-	local Player = exports['qbr-core']:GetPlayer(src)
-	if type(doorID) ~= 'number' then
-			return
-		end
-		if not IsAuthorized(Player.PlayerData.job.name, Config.DoorList[doorID]) then
-			return
-		end
-		DoorInfo[doorID] = {}
-		TriggerClientEvent('qbr-doorlock:setState', -1, doorID, state)
-end)
-
-function IsAuthorized(jobName, doorID)
+local function IsAuthorized(jobName, doorID)
 	for _,job in pairs(doorID.authorizedJobs) do
 		if job == jobName then
 			return true
@@ -35,3 +9,24 @@ function IsAuthorized(jobName, doorID)
 	end
 	return false
 end
+
+RegisterNetEvent('qbr-doorlock:updatedoorsv', function(doorID, state, cb)
+    local src = source
+	local Player = GetPlayer(src)
+	if not IsAuthorized(Player.PlayerData.job.name, Config.DoorList[doorID]) then
+		return TriggerClientEvent('QBCore:Notify', src, 9, Lang:t("error.nokey"), 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
+	else
+		TriggerClientEvent('qbr-doorlock:changedoor', src, doorID, state)
+	end
+end)
+
+RegisterNetEvent('qbr-doorlock:updateState', function(doorID, state, cb)
+    local src = source
+	local Player = GetPlayer(src)
+	if type(doorID) ~= 'number' then return end
+	if not IsAuthorized(Player.PlayerData.job.name, Config.DoorList[doorID]) then
+		return
+	end
+	DoorInfo[doorID] = {}
+	TriggerClientEvent('qbr-doorlock:setState', -1, doorID, state)
+end)
