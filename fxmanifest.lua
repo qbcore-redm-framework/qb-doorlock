5bcadac95bd5293cb4dfd84fb5653930b1f42398
Mr.Cowley <96976330+gononono64@users.noreply.github.com>
2023-07-03 14:41:18 -0500

    Update fxmanifest.lua
    Added version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,12 @@
 fx_version "cerulean"
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
+version '1.0.0'
 server_script 'server/main.lua'
 
 shared_scripts {
 	'@qbr-core/shared/locale.lua',
-    'locales/en.lua',
+	'locales/en.lua',
 	'config.lua'
 }
 
